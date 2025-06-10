1ed934635fb1601bca34271455546a9fd7f02119
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-06-10 16:48:21 +0300

    Add put and putch method

---
+++ main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI, Query, Body
+from fastapi.openapi.docs import get_swagger_ui_html
+import uvicorn
+
+app = FastAPI()
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"}
+]
+
+
+@app.get("/hotels")
+def get_hotels(
+        hotel_id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != hotel_id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def update_hotel(
+    hotel_id: int,
+    title: str = Body(embed=True),
+    name: str = Body(embed=True),
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotel["title"] = title
+            hotel["name"] = name
+        break
+    return {"status": "OK"}
+
+
+@app.patch("/hotels/{hotel_id}")
+def partial_update_hotel(
+    hotel_id: int,
+    title: str | None = Body(None, embed=True),
+    name: str = Body(None, embed=True),
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            if title and name:
+                hotel["title"] = title
+                hotel["name"] = name
+            elif title:
+                hotel["title"] = title
+            elif name:
+                hotel["name"] = name
+            break
+    return {"status": "OK"}
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
