8c4d9cd774e81f454ba93b9c78eccb19d2af8764
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-06-24 15:59:50 +0300

    Update app

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -45,12 +45,17 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     }
 })
 ):
+    # async with async_session_maker() as session:
+    #     add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+    #     await session.execute(add_hotel_stmt)
+    #     await session.commit()
+
+    # return {"status": "OK"}
     async with async_session_maker() as session:
-        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
-        await session.execute(add_hotel_stmt)
+        hotel = await HotelsRepository(session).add(hotel_data)
         await session.commit()
 
-    return {"status": "OK"}
+    return {"status": "OK", "data": hotel}
 
 
 @router.put("/{hotel_id}")
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, insert
 
 
 class BaseRepository:
@@ -16,3 +16,8 @@ class BaseRepository:
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
+    
+    async def add(self, data):
+        stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        result = await self.session.execute(stmt)
+        return result.scalar_one()
