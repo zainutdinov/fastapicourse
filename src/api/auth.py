e4d8971ae180d5fdd326e023a1ef9caa311cb1e5
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-06-28 13:56:59 +0300

    Update app

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException, status
 
 from passlib.context import CryptContext
 
@@ -19,7 +21,14 @@ async def register_user(
     hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        await UsersRepository(session).add(new_user_data)
-        await session.commit()
+        try:
+            await UsersRepository(session).add(new_user_data)
+            await session.commit()
+        except IntegrityError:
+            await session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Пользователь с таким email уже зарегистрирован",
+            )
 
     return {"status": "OK"}
--- src/models/users.py
+++ src/models/users.py
@@ -8,5 +8,5 @@ class UsersOrm(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(200))
+    email: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(200))
