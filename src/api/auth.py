4389d6eaa10b51a3030427f42952be05b177ae4e
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-06-30 12:27:55 +0300

    Update app

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,52 +1,22 @@
-from datetime import datetime, timezone, timedelta
-from fastapi import APIRouter, Response
-from sqlalchemy.exc import IntegrityError
-from fastapi import HTTPException, status
-import jwt
-
-from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
 
 
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd,
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        try:
-            await UsersRepository(session).add(new_user_data)
-            await session.commit()
-        except IntegrityError:
-            await session.rollback()
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail="Пользователь с таким email уже зарегистрирован",
-            )
+        await UsersRepository(session).add(new_user_data)
+        await session.commit()
 
     return {"status": "OK"}
 
@@ -54,14 +24,22 @@ async def register_user(
 @router.post("/login")
 async def login_user(
         data: UserRequestAdd,
-        response: Response
+        response: Response,
 ):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+    
+
+@router.get("/only_auth")
+async def only_auth(
+        request: Request,
+):
+    access_token = request.cookies.get("access_token")
+    return {"access_token": access_token}
