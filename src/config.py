0b761560177d0ec55843d34c9a954cecd8e410b4
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-06-30 12:27:37 +0300

    Update app

--- src/config.py
+++ src/config.py
@@ -13,6 +13,9 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env")
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
 
 settings = Settings()
