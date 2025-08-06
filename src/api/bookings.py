97c7ff73d62a57cac116221ed53b035b3f9e8e8b
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-08-06 17:08:31 +0300

    Ref booking api

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,29 +1,24 @@
 from fastapi import Query, APIRouter, Body
 
 from src.api.dependencies import DBDep, UserIdDep
-from src.schemas.bookings import BookingAdd
+from src.schemas.bookings import BookingAdd, BookingAddRequest
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
 @router.post("/")
-async def create_booking(
-        db: DBDep,
+async def add_booking(
         user_id: UserIdDep,
-        room_id: int,
-        date_from: str = Body(),
-        date_to: str = Body(),
-): 
-    room = await db.rooms.get_one_or_none(id=room_id)
-    price = room.model_dump().get("price", 0)
-    booking_data = BookingAdd(
+        db: DBDep,
+        booking_data: BookingAddRequest,
+):
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    room_price = room.price
+    _booking_data = BookingAdd(
         user_id=user_id,
-        room_id=room_id,
-        date_from=date_from,
-        date_to=date_to,
-        price=price,
+        **booking_data.model_dump(),
+        price=room_price,
     )
-    booking = await db.bookings.add(booking_data)
+    booking = await db.bookings.add(_booking_data)
     await db.commit()
-
     return {"status": "OK", "data": booking}
--- src/schemas/bookings.py
+++ src/schemas/bookings.py
@@ -6,7 +6,6 @@ class BookingAddRequest(BaseModel):
     room_id: int
     date_from: date
     date_to: date
-    price: int
 
 class BookingAdd(BaseModel):
     user_id: int
