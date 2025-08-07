27fc491a8398cb2a5d65cd822bc35f9fc938595c
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-08-07 17:40:12 +0300

    Add paginaation

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -12,21 +12,17 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 async def get_hotels(
         pagination: PaginationDep,
         db: DBDep,
-        # location: str | None = Query(None, description="Локация"),
-        # title: str | None = Query(None, description="Название отеля"),
+        location: str | None = Query(None, description="Локация"),
+        title: str | None = Query(None, description="Название отеля"),
         date_from: date = Query(example="2024-08-01"),
         date_to: date = Query(example="2024-08-10"),
 ):
     per_page = pagination.per_page or 5
-    # return await db.hotels.get_all(
-    #     location=location,
-    #     title=title,
-    #     limit=per_page,
-    #     offset=per_page * (pagination.page - 1)
-    # )
     return await db.hotels.get_filtered_by_time(
         date_from=date_from,
         date_to=date_to,
+        location=location,
+        title=title,
         limit=per_page,
         offset=per_page * (pagination.page - 1)
     )
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -13,14 +13,23 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
+    async def get_filtered_by_time(
             self,
+            date_from: date,
+            date_to: date,
             location,
             title,
             limit,
             offset,
     ) -> list[Hotel]:
-        query = select(HotelsOrm)
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
         if location:
             query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
         if title:
@@ -30,24 +39,6 @@ class HotelsRepository(BaseRepository):
             .limit(limit)
             .offset(offset)
         )
-        print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
 
         return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
-
-    async def get_filtered_by_time(
-            self,
-            date_from: date,
-            date_to: date,
-            limit,
-            offset,
-    ):
-        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
-        hotels_ids_to_get = (
-            select(RoomsOrm.hotel_id)
-            .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-            .limit(limit)
-            .offset(offset)
-        )
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
