857be227a7d04bd5a03a6ca5c6de9a33897f60c7
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-08-08 17:53:07 +0300

    Update rooms api

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -28,3 +28,15 @@ class RoomsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
         return [RoomWithRels.model_validate(model) for model in result.unique().scalars().all()]
+
+    async def get_one_or_none(self, **filter_by):
+        query = (
+            select(self.model)
+            .options(selectinload(self.model.facilities))
+            .filter_by(**filter_by)
+            )
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        return RoomWithRels.model_validate(model)
